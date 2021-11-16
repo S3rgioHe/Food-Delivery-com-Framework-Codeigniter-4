2c5888603d41dce16ab9ebfeff51aa44cbfd839a
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2021-11-16 14:39:42 -0300

    Update index.php

--- app/Views/Admin/Home/index.php
+++ app/Views/Admin/Home/index.php
@@ -9,7 +9,7 @@
   <!-- plugins:css -->
 
   <link rel="stylesheet" href="<?php echo site_url('admin/');?>vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+  <link rel="stylesheet" href="<?php echo site_url('admin/');?>vendors/base/vendor.bundle.base.css">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <link rel="stylesheet" href="<?php echo site_url('admin/');?>vendors/datatables.net-bs4/dataTables.bootstrap4.css">
@@ -578,7 +578,6 @@
   <!-- container-scroller -->
 
   <!-- plugins:js -->
-<<<<<<< HEAD
   <script src="<?php echo site_url('admin/');?>vendors/base/vendor.bundle.base.js"></script>
   <!-- endinject -->
   <!-- Plugin js for this page-->
@@ -598,27 +597,6 @@
   <script src="<?php echo site_url('admin/');?>js/dataTables.bootstrap4.js"></script>
   <!-- End custom js for this page-->
   <script src="<?php echo site_url('admin/');?>js/jquery.cookie.js" type="text/javascript"></script>
-=======
-  <script src="vendors/base/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="vendors/chart.js/Chart.min.js"></script>
-  <script src="vendors/datatables.net/jquery.dataTables.js"></script>
-  <script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="js/off-canvas.js"></script>
-  <script src="js/hoverable-collapse.js"></script>
-  <script src="js/template.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="js/dashboard.js"></script>
-  <script src="js/data-table.js"></script>
-  <script src="js/jquery.dataTables.js"></script>
-  <script src="js/dataTables.bootstrap4.js"></script>
-  <!-- End custom js for this page-->
-  <script src="js/jquery.cookie.js" type="text/javascript"></script>
->>>>>>> fcde01747cbb6ae1b1f2927396a5f0a8b44dc73a
 </body>
 
 </html>
