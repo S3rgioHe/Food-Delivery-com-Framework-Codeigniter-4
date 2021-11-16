8c43795e743fc89a325c1e0d0efa5cec07355ed1
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2021-11-16 16:50:43 -0300

    feat: create new migrate structure

---
+++ app/Database/Migrations/2021-11-16-191605_CriaTabelaUsuarios.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CriaTabelaUsuarios extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+            ],
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
