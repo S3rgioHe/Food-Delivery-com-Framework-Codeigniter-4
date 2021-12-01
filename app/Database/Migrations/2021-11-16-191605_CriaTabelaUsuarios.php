72b72a5efd5f2b7a212d8cb2976f5ef3021166b9
S3rgioHe <54727461+S3rgioHe@users.noreply.github.com>
2021-12-01 16:22:47 -0300

    feat: new migrate for users

--- app/Database/Migrations/2021-11-16-191605_CriaTabelaUsuarios.php
+++ app/Database/Migrations/2021-11-16-191605_CriaTabelaUsuarios.php
@@ -26,10 +26,63 @@ class CriaTabelaUsuarios extends Migration
             'name' => [
                 'type' => 'VARCHAR',
                 'constraint' => '128',
-            ],            
+            ],      
+            'cpf' => [
+                'type' => 'VARCHAR',
+                'constraint' => '15',
+                'null' => true,
+            ],  
+            'telefone' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20',
+            ], 
+            'is_admin' => [
+                'type' => 'BOOLEAN',
+                'null' => false,
+                'default' => false,
+            ],
+            'ativo' => [
+                'type' => 'BOOLEAN',
+                'null' => false,
+                'default' => false,
+            ],
+            'password_hash' => [
+                'constraint' => '64',
+                'unique' => true,
+            ],
+            'ativacao_hash' => [
+                'type' => 'varchar',
+                'constraint' => '64',
+                'unique' => true,
+            ],
+            'reset_hash' => [
+                'type' => 'varchar',
+                'constraint' => '64',
+                'unique' => true,
+            ],
+            'reset_expira_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null,
+            ],
+            'criado_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null,
+            ],
+            'atualizado_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null,
+            ],
+            'deletado_em' => [
+                'type' => 'DATETIME',
+                'null' => true,
+                'default' => null,
+            ],
         ]);
-        $this->forge->addKey('id', true);
-        $this->forge->createTable('users');
+        $this->forge->addKey('id')->addUniqueKey('email');
+        $this->forge->createTable('usuarios');
     }
 
     public function down()
